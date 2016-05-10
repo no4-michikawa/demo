36747ce7fe2a581996b14daca885ae2a55507fdd
ichikawa <ichikawa@xxxg-01w.local>
2016-05-10 13:25:03 +0900

    test

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by ichikawa on 2016/05/10.
+//  Copyright © 2016年 ichikawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
